8a73989150f462ebb7ce627b8f76483980954e10
Roberto Zegers <rzegers@puc.cl>
2022-12-15 13:19:58 +0000

    go_to_pose: add initial ROS2 pkg files, Readme & License

---
+++ go_to_pose_controller/README.md
@@ -0,0 +1,25 @@
+# The go_to_pose_controller ROS2 package
+
+- Author: Roberto Zegers
+- Date: December 2022
+- License: BSD-3-Clause
+
+## Description
+
+- Minimal go to pose controller node (Python)
+
+
+## Instructions
+
+Inside your ROS2 workspace root build with:  
+
+`colcon build --packages-select go_to_pose_controller`   
+
+After compiling, source the workspace otherwise you will get the "Package 'go_to_pose_controller' not found" error message:  
+
+`source install/setup.bash`  
+
+
+
+## Dependencies
+- ROS2 Galactic  
---
+++ go_to_pose_controller/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'go_to_pose_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='TODO: Package description',
+    license='BSD-3-Clause',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
