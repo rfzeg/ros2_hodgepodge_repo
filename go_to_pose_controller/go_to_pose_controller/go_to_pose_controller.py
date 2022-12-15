f57b8b3f39c21d3ffdcafe5b906cd713c7cba8c5
Roberto Zegers <rzegers@puc.cl>
2022-12-15 15:08:11 +0000

    Add odometry subscriber & transform to pose in xy plane

--- go_to_pose_controller/go_to_pose_controller/go_to_pose_controller.py
+++ go_to_pose_controller/go_to_pose_controller/go_to_pose_controller.py
@@ -2,6 +2,8 @@ import traceback as tb
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf_transformations import euler_from_quaternion
 
 
 class GoToPoseController(Node):
@@ -19,6 +21,14 @@ class GoToPoseController(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.cmd_vel_msg = Twist()
 
+        self.odom_subscription = self.create_subscription(
+            Odometry, 'odom', self.odom_callback, 10)
+        # prevent unused variable warning
+        self.odom_subscription
+
+        # attribute to store odometry pose (x,y,yaw) in odometry frame
+        self.robot_pose_in_xy_plane = None
+
     def timer_callback(self):
         """
         timer callback method
@@ -29,6 +39,40 @@ class GoToPoseController(Node):
         self.cmd_vel_msg.angular.z = 0.2
         # publish the message to the topic
         self.cmd_vel_publisher.publish(self.cmd_vel_msg)
+
+    def odom_callback(self, msg):
+        """
+        Odometry pose orientation in rad range of (-pi,pi)
+        """
+        self.robot_pose_in_xy_plane = self.pose_object_to_pose_2d(msg.pose)
+
+        self.get_logger().info(
+            f"Robot pose in '{msg.header.frame_id}' frame (x: {self.robot_pose_in_xy_plane[0]:.2f}, y: {self.robot_pose_in_xy_plane[1]:.2f}, theta: {self.robot_pose_in_xy_plane[2]:.2f})",
+            throttle_duration_sec=0.5  # Throttle logging frequency to max 2Hz
+        )
+
+    @staticmethod
+    def pose_object_to_pose_2d(ros_pose_obj):
+        quaternion = (
+            ros_pose_obj.pose.orientation.x,
+            ros_pose_obj.pose.orientation.y,
+            ros_pose_obj.pose.orientation.z,
+            ros_pose_obj.pose.orientation.w
+        )
+
+        # convert quaternion to euler angles
+        _, _, yaw = euler_from_quaternion(quaternion)
+
+        pose_in_plane = (
+            # x position in meters (pose obj. frame)
+            ros_pose_obj.pose.position.x,
+            # x position in meters (pose obj. frame)
+            ros_pose_obj.pose.position.y,
+            # orientation in radians (pose obj. frame)
+            yaw
+        )
+
+        return pose_in_plane
 
     def stop(self):
         cmd_vel = Twist()
